63a9342864a37f89b83dd72796685a570357892a
Mindy Preston <mindy.preston@docker.com>
2016-04-20 19:20:38 +0100

    Calculate and set checksum value for ICMP echo requests.
    Signed-off-by: Mindy Preston <mindy.preston@docker.com>

--- lib/icmp/icmpv4_print.ml
+++ lib/icmp/icmpv4_print.ml
@@ -1,10 +1,14 @@
-let echo_request id seq =
+let echo_request ?payload id seq =
   let open Icmpv4_wire in
-  (* TODO: can this just be an appropriately-sized cstruct? *)
-  let buf = Io_page.(get 1 |> to_cstruct) in
-  let buf = Cstruct.set_len buf sizeof_icmpv4 in
-  set_icmpv4_ty buf 0x08;
-  set_icmpv4_code buf 0x00;
-  set_icmpv4_seq buf seq;
-  set_icmpv4_id buf id;
-  buf
+  let header = Cstruct.create sizeof_icmpv4 in
+  set_icmpv4_ty header 0x08;
+  set_icmpv4_code header 0x00;
+  set_icmpv4_csum header 0x0000;
+  set_icmpv4_seq header seq;
+  set_icmpv4_id header id;
+  let packet = match payload with
+    | Some payload -> Cstruct.append header payload
+    | None -> header
+  in
+  set_icmpv4_csum header (Tcpip_checksum.ones_complement_list [ packet ]);
+  packet
